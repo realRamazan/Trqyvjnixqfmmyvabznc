5628df51a33ef2f75cfde7b8c0a8191776275135
realRamazan <ramazan.baktybaiuly@gmail.com>
2023-10-23 14:23:20 +0600

    Create project

---
+++ src/main/java/com/example/Trqyvjnixqfmmyvabznc/TrqyvjnixqfmmyvabzncApplication.java
@@ -0,0 +1,13 @@
+package com.example.Trqyvjnixqfmmyvabznc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrqyvjnixqfmmyvabzncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrqyvjnixqfmmyvabzncApplication.class, args);
+	}
+
+}
